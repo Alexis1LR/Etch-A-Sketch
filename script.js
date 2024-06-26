d2db65ff9d998fb1940e1ecd590671709747bf39
Alexis1LR <101027285+Alexis1LR@users.noreply.github.com>
2024-06-25 21:52:07 -0400

    allow ability to generate new grids based on user input

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 
-function create_grid() {
-    for (j = 0; j < 16; j++) {
-        //mini_container create containers for each coloumn of boxes 
-        //once all the boxes are created for that column, we create another container for the next column of boxes
-        const mini_container = document.createElement("div");
-        for (i = 0; i < 16; i++) { //loop that creates 16 grid boxes
-            const box = document.createElement("div");
-            box.classList.add("box");
-            box.style.display = "flex";
-            //the next 2 lines give each box set dimensions for a clear display of the box
-            box.style.width = "20px";
-            box.style.height = "20px";
-            box.style.border = "solid";
-            mini_container.appendChild(box);
+function create_grid(size = 0) {
+    if (size == 0) {
+        for (j = 0; j < 16; j++) {
+            //mini_container create containers for each coloumn of boxes 
+            //once all the boxes are created for that column, we create another container for the next column of boxes
+            const mini_container = document.createElement("div");
+            mini_container.classList.add("mini-container")
+            for (i = 0; i < 16; i++) { //loop that creates 16 grid boxes
+                const box = document.createElement("div");
+                box.classList.add("box");
+                box.style.display = "flex";
+                //the next 2 lines give each box set dimensions for a clear display of the box
+                box.style.width = "20px";
+                box.style.height = "20px";
+                box.style.border = "solid";
+                mini_container.appendChild(box);
+            }
+            main_container.appendChild(mini_container);
         }
-        main_container.appendChild(mini_container);
     }
+    else {
+        for (j = 0; j < size; j++) {
+            //mini_container create containers for each coloumn of boxes 
+            //once all the boxes are created for that column, we create another container for the next column of boxes
+            const mini_container = document.createElement("div");
+            mini_container.classList.add("mini-container")
+            for (i = 0; i < size; i++) { //loop that creates 16 grid boxes
+                const box = document.createElement("div");
+                box.classList.add("box");
+                box.style.display = "flex";
+                //the next 2 lines give each box set dimensions for a clear display of the box
+                box.style.width = "20px";
+                box.style.height = "20px";
+                box.style.border = "solid";
+                mini_container.appendChild(box);
+            }
+            main_container.appendChild(mini_container);
+        }
+    }
+
 }
+
 
 const main_container = document.querySelector(".container");
 main_container.style.display = "flex";
+const grid_button = document.querySelector(".new-grid");
+grid_button.addEventListener("click", grid_request);
 create_grid();
-apply_hover();
+main_container.addEventListener("mouseover", apply_color)
+//need to have the event listener outside the function so that it runs everytime 
 
 //when the mouse hovers over a box, it applies a random color to the box 
-function apply_hover() {
+function apply_color() {
     const boxes = document.querySelectorAll(".box");
     boxes.forEach(function (box) {
         //the anonymous function generates a random color and applies it to each box element 
@@ -36,3 +63,21 @@ function apply_hover() {
     })
 }
 
+function grid_request() {
+    let size = prompt("How many squares would you like each side to be? (2-100)");
+    clear_grid();
+    create_grid(size);
+}
+
+//clear_grid clears the current grid that is in the window
+function clear_grid() {
+    count = main_container.childElementCount;
+    for (i = 0; i < count; i++) {
+        //we use a loop to get rid of each child of the main_container, which will clear the whole grid 
+        const mini_container = document.querySelector(".mini-container");
+        main_container.removeChild(mini_container);
+
+
+    }
+
+}
